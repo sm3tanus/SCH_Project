ffa37b5fdc9990fb48dfa47a81158af27b732adb
sm3tanus <smetanus3@gmail.com>
2023-11-23 15:11:24 +0300

    sss

--- SCH_Project/DBconnection/Connection.cs
+++ SCH_Project/DBconnection/Connection.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace SCH_Project.DBconnection
 {
     public class Connection
+
     {
         public static TaskManagerBySCHToDoListEntities taskManager = new TaskManagerBySCHToDoListEntities();
     }
