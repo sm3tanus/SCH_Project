92bd92e514c55457877d524ca85198e5b8434274
sm3tanus <smetanus3@gmail.com>
2023-11-24 12:47:53 +0300

    сделала логику авторизации и регистрации лианааааа

--- SCH_Project/Pages/AuthorizationPage.xaml.cs
+++ SCH_Project/Pages/AuthorizationPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using SCH_Project.Dbconnection;
 
 namespace SCH_Project.Pages
 {
@@ -10,42 +11,57 @@ namespace SCH_Project.Pages
     /// </summary>
     public partial class AuthorizationPage : Page
     {
-        //public static List<User> users {  get; set; }
+        public static List<User> users { get; set; }
         public AuthorizationPage()
         {
             InitializeComponent();
         }
-        //public User newUser = new User();
-        //private void EnterBt_Click(object sender, System.Windows.RoutedEventArgs e)
-        //{
-        //    if (TabControl.SelectedIndex == 0)
-        //    {
-        //        try
-        //        {
-        //            string login = loginTb.Text.Trim();
-        //            string password = PasswordTb.Password.Trim();
-        //            var user = Connection.taskManager.User.Where(i => i.Login == login && i.Password == password).ToList();
-        //            if (user != null)
-        //            {
-        //                MessageBox.Show("Welcome");
-        //            }
-        //        }
-        //        catch
-        //        {
-        //            MessageBox.Show("Error");
-        //        }
-        //    }
-        //    else
-        //    {
-        //        try
-        //        {
-        //            newUser.Login = loginTb.Text.Trim();
-        //            newUser.Password = PasswordTb.Password.Trim();
-        //        }
-        //        catch
-        //        {
-        //            MessageBox.Show("Error");
-        //        }
-        //    }
+        public User newUser = new User();
+        private void EnterBt_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            {
+                try
+                {
+                    string login = loginTb.Text.Trim();
+                    string password = PasswordTb.Password.Trim();
+                    var user = Connection.taskManager.User.Where(i => i.Login == login && i.Password == password).ToList();
+                    if (user != null)
+                    {
+                        MessageBox.Show("Welcome");
+                        NavigationService.Navigate(new MainMenuPage());
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+        }
+
+        private void Regbt_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                if (PasswordReg.Password.Trim() == PasswordConfirmReg.Password.Trim())
+                {
+                    newUser.Login = LoginReg.Text.Trim();
+                    newUser.Password = PasswordReg.Password.Trim();
+                    Connection.taskManager.User.Add(newUser);
+                    Connection.taskManager.SaveChanges();
+                    MessageBox.Show("Welcome");
+                    NavigationService.Navigate(new MainMenuPage());
+                }
+                else
+                {
+                    LoginReg.Text = " ";
+                    PasswordReg.Password = " ";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
         }
     }
+}
