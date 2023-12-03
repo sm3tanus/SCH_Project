b61ef0289c17647eb1c962191d90326d813fb1e3
meaucat <yuuummm55@gmail.com>
2023-12-03 20:23:43 +0300

    SubmitAnApplicationPage сделана димими

---
+++ SCH_Project/Pages/SubmitAnApplicationPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SCH_Project.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для SubmitAnApplicationPage.xaml
+    /// </summary>
+    public partial class SubmitAnApplicationPage : Page
+    {
+        public SubmitAnApplicationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
